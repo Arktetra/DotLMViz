46cc089e24c5b4169e2c6e5a5a4f7e36e8af6957
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-09 22:35:24 +0545

    add: flowbite-svelte-icons

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import ThemeButton from "./components/ThemeButton.svelte";
+  import { HeartSolid } from 'flowbite-svelte-icons';
 
   let rand: string = $state("-1");
 
@@ -16,5 +17,7 @@
 <section class="min-w-full min-h-screen flex flex-col justify-evenly items-center">
   <h1 class="font-bold uppercase text-theme">Your number is {rand}!</h1>
 
-  <ThemeButton label="Click" clickEvent={getRand} />
+  <ThemeButton label="Click" clickEvent={getRand} >
+    <HeartSolid fill="red" class="inline-block" />
+  </ThemeButton>
 </section>
