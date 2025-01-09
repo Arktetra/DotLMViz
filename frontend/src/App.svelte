c091f8adec7a3ca7ac8571be07540106ce47d8ff
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-09 23:05:42 +0545

    add: ThemeInputField Component & test in app

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,8 +2,10 @@
   import { onMount } from "svelte";
   import ThemeButton from "./components/ThemeButton.svelte";
   import { HeartSolid } from 'flowbite-svelte-icons';
+  import ThemeInputField from "./components/ThemeInputField.svelte";
 
   let rand: string = $state("-1");
+  let inputField: string = $state("None");
 
   function getRand() {
     fetch("/api/rand")
@@ -12,6 +14,10 @@
   }
 
   onMount(getRand)
+
+  function inputChange(val : any) {
+    inputField = val;
+  } 
 </script>
 
 <section class="min-w-full min-h-screen flex flex-col justify-evenly items-center">
@@ -20,4 +26,16 @@
   <ThemeButton label="Click" clickEvent={getRand} >
     <HeartSolid fill="red" class="inline-block" />
   </ThemeButton>
+
+  {inputField}
+  
+  <div>
+    <ThemeInputField inputEvent={inputChange} />
+    <ThemeButton label="Generate" />
+  </div>
+
+  <div>
+    <ThemeInputField label="Password" type="password" inputEvent={inputChange} />
+    <ThemeButton label="pass" />
+  </div>
 </section>
---
+++ frontend/src/components/ThemeInputField.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    const { label = "Label", type = "text", placeholder = "Enter text here..", inputEvent = null } = $props()
+</script>
+
+
+<label class="text-theme">{label}:</label>
+<input 
+    type={type}
+    placeholder={placeholder}
+    oninput={(e : any) => inputEvent(e.target.value)}
+    class="min-w-[20rem] p-[0.25rem] px-2 m-2 outline-none border border-theme text-lg text-theme rounded-sm"
+/>
