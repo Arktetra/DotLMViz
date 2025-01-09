ce54341d914cbc48efc126139b64f24e3c06a846
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-09 23:04:55 +0545

    update: keyword change from let to const & style

--- frontend/src/components/ThemeButton.svelte
+++ frontend/src/components/ThemeButton.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-  import { scale } from "svelte/transition";
+    import { scale } from "svelte/transition";
 
-    let { label, children = null, clickEvent = null } = $props();
+    const { label, children = null, clickEvent = null } = $props();
 </script>
 
-<button transition:scale={{duration : 500}} class="bg-theme text-theme-w p-2 px-4 rounded-md hover:scale-105 hover:shadow-md active:bg-theme-alt transition-transform duration-300" onclick={() => clickEvent ? clickEvent() : () => {}}>
+<button 
+    transition:scale={{duration : 500}} 
+    onclick={() => clickEvent ? clickEvent() : () => {}}
+    class="bg-theme text-theme-w p-2 px-4 rounded-md hover:scale-105 hover:shadow-md active:bg-theme-alt transition-transform duration-300"
+>
     {label}
     {#if children}
         <svelte:component this={children} />
