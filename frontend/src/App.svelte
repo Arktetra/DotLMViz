5f49ef3bd57f3ffa0e45aeec36c86bcd80d864a1
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-10 16:35:19 +0545

    add: FullScreenOverlay, Popup Components & test

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,9 +3,11 @@
   import ThemeButton from "./components/ThemeButton.svelte";
   import { HeartSolid } from 'flowbite-svelte-icons';
   import ThemeInputField from "./components/ThemeInputField.svelte";
+  import Popup from "./components/Popup.svelte";
 
   let rand: string = $state("-1");
   let inputField: string = $state("None");
+  let popupOpen: boolean = $state(false);
 
   function getRand() {
     fetch("/api/rand")
@@ -18,16 +20,30 @@
   function inputChange(val : any) {
     inputField = val;
   } 
+
+  function setPopupState(state : boolean)
+  {
+    popupOpen = state;
+  }
 </script>
 
 <section class="min-w-full min-h-screen flex flex-col justify-evenly items-center">
   <h1 class="font-bold uppercase text-theme">Your number is {rand}!</h1>
 
-  <ThemeButton label="Click" clickEvent={getRand} >
+  <ThemeButton label="Click" clickEvent={() => setPopupState(!popupOpen)} >
     <HeartSolid fill="red" class="inline-block" />
   </ThemeButton>
 
   {inputField}
+
+  {#if popupOpen}
+      <Popup onClose={() => setPopupState(false)} >
+        <div class="text-center min-h-[10rem] min-w-[20rem]">
+          <h1 class="text-xl font-bold uppercase underline my-2">This is popup</h1>
+          <span>These are children house by popup</span>
+        </div>
+      </Popup>
+  {/if}
   
   <div>
     <ThemeInputField inputEvent={inputChange} />
---
+++ frontend/src/components/Popup.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { CloseOutline } from "flowbite-svelte-icons";
+    import FullScreenOverlay from "./FullScreenOverlay.svelte";
+
+    const { onClose = null, style = "", children = null } = $props();
+</script>
+
+<FullScreenOverlay
+    clickEvent={onClose}
+>
+<div 
+    class={"min-w-[5rem] min-h-[5rem] p-2 pt-5 z-20 bg-theme text-theme-w rounded-xl relative " + style} on:click|stopPropagation
+>
+    <CloseOutline
+        class="w-5 h-5 absolute -top-0 -right-0 cursor-pointer text-red-600 bg-theme-w rounded-md hover:scale-105 transition-transform duration-300"
+        onclick={() => onClose()}
+    />
+    
+    {#if children}
+        {@render children()}
+    {/if}
+</div>
+</FullScreenOverlay>
