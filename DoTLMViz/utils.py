88ac9421aa23fc47a2c97a99956261d8d7dd9b1d
Arktetra <sameershrestha.sm@gmail.com>
2025-01-17 21:21:30 +0545

    update notebook

--- DoTLMViz/utils.py
+++ DoTLMViz/utils.py
@@ -1,5 +1,9 @@
 import torch
 
+import torch.nn.functional as F
+
+from jaxtyping import Float
+
 
 def standardize(X: torch.Tensor) -> torch.Tensor:
     """Standardize the given tensor to have mean of 0 and standard deviation of 1.
@@ -20,6 +24,17 @@ def compare(a: torch.Tensor, b: torch.Tensor) -> float:
     """
     comparison = torch.isclose(a, b, atol=1e-4, rtol=1e-3)
     return comparison.sum() / comparison.numel()
+
+
+def cross_entropy_loss(
+    logits: Float[torch.Tensor, "batch seq d_vocab"], tokens: Float[torch.Tensor, "batch seq"]
+) -> torch.Tensor:
+    """
+    Returns cross entropy loss for the given logits and tokens.
+    """
+    log_probs: Float[torch.Tensor, "batch seq d_vocab"] = F.log_softmax(logits, dim=-1)
+    pred_log_probs: Float[torch.Tensor, "batch seq"] = torch.gather(log_probs[:, :-1], -1, tokens[:, 1:, None])[..., 0]
+    return -pred_log_probs.mean()
 
 
 def predict_next_token(logits: torch.Tensor) -> torch.Tensor:
