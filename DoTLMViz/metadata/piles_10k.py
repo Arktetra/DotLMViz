494f95b4f63583d183c703fbb390d5b8c5557035
Arktetra <sameershrestha.sm@gmail.com>
2025-01-18 13:56:00 +0545

    change to src layout

--- DoTLMViz/metadata/piles_10k.py
+++
@@ -1,5 +0,0 @@
-import DoTLMViz.metadata.shared as shared
-
-DL_DATA_DIRNAME = shared.DOWNLOADED_DATA_DIRNAME / "Piles10k"
-RAW_DATA_DIRNAME = shared.DATA_DIRNAME / "raw" / "Piles10k"
-PROCESSED_DATA_DIRNAME = shared.DATA_DIRNAME / "processed"
