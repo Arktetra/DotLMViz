acbb949462296c1ad6b9165b5afbf640f37bff68
Arktetra <sameershrestha.sm@gmail.com>
2025-01-19 22:52:22 +0545

    update api routes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 				target: 'http://127.0.0.1:5000',
 				timeout: 10000	// don't abandon the connection until 10 seconds have passed, sometimes loading model takes around 5 seconds.
 			},
-			'/model/pred': 'http://127.0.0.1:5000',
+			'/model/dist': 'http://127.0.0.1:5000',
 			'/model/run': 'http://127.0.0.1:5000',
 		}
 	}
