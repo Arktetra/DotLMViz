a546945396684153dddec0a2c4c77ad9b202f420
Arktetra <sameershrestha.sm@gmail.com>
2025-01-19 22:56:16 +0545

    add files for creating BarChart

--- frontend/src/modules/OutputBlock.svelte
+++ frontend/src/modules/OutputBlock.svelte
@@ -1,23 +1,9 @@
 <script lang="ts">
 	import SideDrawer from '../components/SideDrawer.svelte';
 	import ThemeInputSlider from '../components/ThemeInputSlider.svelte';
-	import { activeComponent } from '../state.svelte';
+	import BarChart from '../dataviz/BarChart.svelte';
+	import { activeComponent, data } from '../state.svelte';
 
-	const predNextToken = async () => {
-		try {
-			return await fetch('/model/pred')
-				.then((res) => res)
-				.then((res) => {
-					let logits = res.json();
-					console.log(logits);
-					// return logits;
-				})
-				.catch((error) => console.log("Could not predict the next token " + error));
-		} catch (error) {
-			console.log("Unable to fetch " + error);
-			return;
-		}
-	}
 </script>
 
 <SideDrawer width={'25rem'}>
@@ -37,11 +23,11 @@
 		<div
 			class="flex min-h-[15rem] w-full flex-col items-center justify-evenly rounded-md bg-theme-g-alt p-3 shadow-inner shadow-theme-g-alt"
 		>
-			<div class="chart w-full text-right text-ti font-light">
+			<div
+				class="chart w-full text-right text-ti font-light"
+			>
 				{#if activeComponent.name === "Generate"}
-					<span class="text-md my-4 block text-center font-bold text-theme underline"
-						>Chart here.. {activeComponent.name} {predNextToken()}</span
-					>
+					<BarChart tokens={data.tokenProbMappings} />
 				{/if}
 			</div>
 		</div>
--- frontend/src/state.svelte.ts
+++ frontend/src/state.svelte.ts
@@ -1,3 +1,7 @@
 export const activeComponent = $state({
     name: 'Output Distribution'
 });
+
+export const data = $state({
+    tokenProbMappings: []
+})
