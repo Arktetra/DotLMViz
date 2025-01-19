d71156b424c79017ee86b291d21c40423bd95799
Arktetra <sameershrestha.sm@gmail.com>
2025-01-19 22:54:44 +0545

    update callback function on Generate

--- frontend/src/modules/InputBlock.svelte
+++ frontend/src/modules/InputBlock.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import ThemeInputField from '../components/ThemeInputField.svelte';
 	import ThemeButton from '../components/ThemeButton.svelte';
-	import { activeComponent } from '../state.svelte';
+	import { activeComponent, data } from '../state.svelte';
 
 	let { inpEventCb = null, value = $bindable() } = $props();
 
 	let inpText: string = $state('');
+	// let tokenProbMappings: { name: string, prob: number }[] = [];
 
 	const randomInpText = () => {
 		inpEventCb('alpha beta gamma delta eta zeta epsilon');
@@ -30,11 +31,35 @@
 			return;
 		}
 	}
+
+	const getDist = async () => {
+		try {
+			const res = await fetch('/model/dist');
+
+			if (!res.ok) {
+				throw new Error(`Response status: ${res.status}`);
+			}
+
+			data.tokenProbMappings = await res.json();
+		} catch (error: any) {
+			console.error(error.message);
+			return;
+		}
+	}
+
+	const runAndGetDist = async () => {
+		await runModel();
+		await getDist();
+	}
+
+	$effect(() => {
+		$inspect(data.tokenProbMappings);
+	})
 </script>
 
 <div>
 	<ThemeInputField {inpEventCb} {value} maxlen={60} />
-	<ThemeButton label="Generate" clickEventCb={runModel} />
+	<ThemeButton label="Generate" clickEventCb={runAndGetDist} />
 	<ThemeButton label="Clear" clickEventCb={() => (value = '')} />
 	<ThemeButton label="Load Example" clickEventCb={randomInpText} />
 </div>
