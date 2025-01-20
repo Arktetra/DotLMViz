451c7834fbd8c7fab93018a7b80833679ff2fddc
Arktetra <sameershrestha.sm@gmail.com>
2025-01-20 20:45:46 +0545

    add component to draw circle

---
+++ frontend/src/dataviz/Circle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let {x, y, r, fill} = $props();
+</script>
+
+<circle
+    cx={x}
+    cy={y}
+    r={r}
+    fill={fill}
+/>
+
+<style>
+    circle {
+        opacity: 0.8;
+    }
+</style>
