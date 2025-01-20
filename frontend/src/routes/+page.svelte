33dcbae00b88d146d894854d4d83117145e3f4f7
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-20 20:09:02 +0545

    add: view testing with toggle component (temporary)

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,10 +10,13 @@
 	import { runModel, loadModel, predNextToken, getAttnScores } from './fetch.svelte';
 	import { InitEventMap } from '../eventstate.svelte';
 	import { active_model } from '../state.svelte';
+	import ThemeToggle from '../components/ThemeToggle.svelte';
 
 	let tokens: string[] = $state([]);
 	let inpText: string = $state('');
 	let activeTokenInd: number = $state(0);
+	// this will hold which view it is, 0 (false) - black box view, 1 (true) - white box view
+	let viewMode: boolean = $state(false);
 
 	let act_name = 'pattern';
 	let layer_name = 'attn';
@@ -57,9 +60,19 @@
 			borderSize={'1px'}
 			inStyle="min-w-[50%] m-2 py-2 pt-8 flex-row justify-between space-x-10"
 		>
-			<EmbeddingBlock />
-			<TransformerBlocks />
-			<UnembeddingBlock />
+			{#if viewMode}
+				<EmbeddingBlock />
+				<TransformerBlocks />
+				<UnembeddingBlock />
+			{:else}
+				<div class="min-w-[40rem] min--h-[20rem] grid grid-cols-4">
+					<DottedBlockBase label="Embedding"><span class="min-w-[5rem] min-h-[5rem]"></span></DottedBlockBase>
+					<div class="col-span-2 relative">
+						<DottedBlockBase label="Transformer Blocks" titStyle="w-full text-center text-sm"><span class="min-w-[5rem] min-h-[10rem]"></span></DottedBlockBase>
+					</div>
+					<DottedBlockBase label="Unembedding"><span class="min-w-[5rem] min-h-[5rem]"></span></DottedBlockBase>
+				</div>
+			{/if}
 		</DottedBlockBase>
 		<DottedBlockBase label="Output">
 			<div class="flex min-h-[5rem] min-w-[5rem] flex-col items-center justify-evenly">
@@ -71,3 +84,4 @@
 	<InputBlock bind:value={inpText} inpEventCb={onInpChange} />
 </section>
 <OutputBlock />
+<ThemeToggle bind:state={viewMode} style="fixed bottom-5 left-[1rem] z-50" leftlabel="Detailed" rightlabel="Overview"/>
