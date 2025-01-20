f65d5a9e4df080bf7a5dc4d8fc9f593901379f8b
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-20 09:27:22 +0545

    add: active_model, global_state shared state

--- frontend/src/state.svelte.ts
+++ frontend/src/state.svelte.ts
@@ -5,3 +5,14 @@ export const activeComponent = $state({
 export const data = $state({
     tokenProbMappings: []
 })
+
+// All the model information name, embed_size, ...
+export const active_model = $state({
+    model_name: "gpt2-small"
+})
+
+// All the global state here
+// #note: currently this is not to be used, left to configure
+export const global_state = $state({
+    isModelLoaded: false,
+})
