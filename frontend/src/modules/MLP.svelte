6c922c5838b446112b4b028e5cf05331f3cebbc1
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-20 10:32:51 +0545

    add: AttentionHeads, MLP module basic

---
+++ frontend/src/modules/MLP.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+    import BlockBase from "../components/BlockBase.svelte";
+
+	let _activeInd = $state(0);
+
+    $effect(() => {
+        console.log("Attention head clicked " + _activeInd);
+    })
+</script>
+
+<h1 class="w-full p-4 mb-5 text-2xl bg-theme-g text-theme font-bold text-center uppercase">Multi-level Perceptron (MLP)</h1>
+<BlockBase label="Linear" href="/read/mlp-linear" />
+<BlockBase label="new_gelu" href="/read/mlp-new-gelu" />
+<BlockBase label="Linear" href="/read/mlp-linear" />
+<br />
