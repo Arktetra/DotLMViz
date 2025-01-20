a0f2511db28b08dc0501eff8e2c11e91b8027dc2
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-20 20:08:20 +0545

    add: ThemeToggle component (range based)

---
+++ frontend/src/components/ThemeToggle.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { state = $bindable(), leftlabel = "", rightlabel = "", style="" } = $props()
+</script>
+
+<div class="flex flex-row justify-evenly items-center text-theme text-sm font-bold {style}">
+    {#if leftlabel}
+        <label>{leftlabel}</label>
+    {/if}
+    <input type="range" class="mx-2 w-7 inline-block cursor-pointer transition-all duration-200 accent-theme rounded-md" min={0} max={1} step={1} onchange={() => state = !state} />
+    {#if rightlabel}
+        <label>{rightlabel}</label>
+    {/if}
+</div>
