b32361d72ace4d2a1a2469da8eececd77e0eba5f
Arktetra <sameershrestha.sm@gmail.com>
2025-01-21 18:58:26 +0545

    add callback functions to the embeddings

--- frontend/src/modules/EmbeddingBlock.svelte
+++ frontend/src/modules/EmbeddingBlock.svelte
@@ -1,22 +1,38 @@
 <script>
 	import DottedBlockBase from '../components/DottedBlockBase.svelte';
 	import BlockBase from '../components/BlockBase.svelte';
+	import { getAct } from '../routes/fetch.svelte';
+	import { activeComponent } from '../state.svelte';
+
+	const getEmbedOutput = () => {
+		getAct("embed", null, null);
+		activeComponent.name = "embed";
+	}
+
+	const getPosEmbedOutput = () => {
+		getAct("pos_embed", null, null);
+		activeComponent.name = "pos_embed";
+	}
 
 	const _embeddings = [
 		{
 			label: 'Token Embedding',
-			href: '/read/token-embedding'
+			href: '/read/token-embedding',
+			callback: getEmbedOutput
 		},
 		{
 			label: 'Positional Embedding',
-			href: '/read/positional-embedding'
+			href: '/read/positional-embedding',
+			callback: getPosEmbedOutput
 		}
 	];
+
+
 </script>
 
 <DottedBlockBase label="Embeddings" inStyle="flex-col">
 	{#each _embeddings as item}
-		<BlockBase label={item.label} href={item.href}>
+		<BlockBase label={item.label} href={item.href} clickEventCb={item.callback}>
 			<h1 class="bg-theme p-4">{item.label}</h1>
 		</BlockBase>
 	{/each}
