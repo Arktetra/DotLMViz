4a370285b346e7b7ab84fdaa8ed7749f53aa42ed
purushottam061 <purugajurel77@gmail.com>
2025-01-23 22:27:00 +0545

    Add content.ts and update page.svelte

--- frontend/src/routes/read/[title]/+page.svelte
+++ frontend/src/routes/read/[title]/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { content } from '$lib/content';
+
 	console.log(page.params);
 	const title = page.params.title.split('-').join(' ');
+	const description = content[page.params.title] ?? 'NO information available for this topic!!';
 </script>
 
 <div class="flex flex-col items-center justify-evenly">
 	<span class="text-md uppercase">Readings</span>
 	<div
-		class="my-10 min-h-[80vh] w-[80%] rounded-sm border-2 border-theme-g p-10 text-justify text-lg tracking-wider shadow-lg"
+		class="border-theme-g my-10 min-h-[80vh] w-[80%] rounded-sm border-2 p-10 text-justify text-lg tracking-wider shadow-lg"
 	>
-		<h1 class="mb-10 text-center text-4xl font-extrabold uppercase text-theme">{title}</h1>
+		<h1 class="text-theme mb-10 text-center text-4xl font-extrabold uppercase">{title}</h1>
 		<p class="h-full w-full">
-			This is the page containing the information about <b>{title}</b>. Let's consider
-			<b>{title}</b> is used in this page to do this... and that...
+			{description}
 		</p>
 	</div>
 </div>
