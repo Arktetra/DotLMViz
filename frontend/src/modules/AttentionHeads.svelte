625c93560d287a7bf6f527542eadb4529ad2960b
Arktetra <sameershrestha.sm@gmail.com>
2025-01-24 13:34:14 +0545

    add a callback function for getting attn pattern

--- frontend/src/modules/AttentionHeads.svelte
+++ frontend/src/modules/AttentionHeads.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 
     import ThemeNumberOptions from "../components/ThemeNumberOptions.svelte";
+	import { getAct } from "../routes/fetch.svelte";
+	import { activeComponent, global_state } from "../state.svelte";
 
-	let _activeInd = $state(0);
-
-    $effect(() => {
-        console.log("Attention head clicked " + _activeInd);
-    })
+    /**
+     * A function which will fetch the attention pattern and store it
+     * in the global state `global_state.data`.
+     */
+    const getAttnPattern = async () => {
+        await getAct("pattern", "attn", global_state.active_block);
+        activeComponent.name = "attn";
+    }
 </script>
 
 <h1 class="w-full p-4 text-2xl bg-theme-g text-theme font-bold text-center uppercase">Attention Heads</h1>
-<span class="text-theme font-bold my-5">Active Head : {_activeInd}</span>
-<ThemeNumberOptions count={12} bind:activeIndex={_activeInd} style="grid grid-cols-3 gap-2 p-2" />
+<span class="text-theme font-bold my-5">Active Head : {global_state.active_head}</span>
+<ThemeNumberOptions
+    count={12}
+    bind:activeIndex={global_state.active_head}
+    style="grid grid-cols-3 gap-2 p-2"
+    clickEventCb={getAttnPattern}
+/>
