93d01f9cf4858a568e7b503f4ac98783db7a40f7
Arktetra <sameershrestha.sm@gmail.com>
2025-01-27 16:38:53 +0545

    add some commonly used types

---
+++ frontend/src/types.svelte.ts
@@ -0,0 +1,14 @@
+type ScatterPlotPoint = {
+    x: number,
+    y: number,
+    token: string
+}
+
+type HeatMapPoint = {
+    x: number,
+    y: number,
+    score: number
+}
+
+type ScatterPlotData = ScatterPlotPoint[]
+type HeatMapData = HeatMapPoint[]
