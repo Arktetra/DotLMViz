dad68a4c0201bf92d0cc3790add1084ebd5deeda
Arktetra <sameershrestha.sm@gmail.com>
2025-01-27 16:40:32 +0545

    update global_state and add input state

--- frontend/src/state.svelte.ts
+++ frontend/src/state.svelte.ts
@@ -11,11 +11,24 @@ export const active_model = $state({
 	model_name: 'gpt2-small'
 });
 
+export const input = $state({
+	isChanged: false,
+	text: ""
+});
+
 // All the global state here
 // #note: currently this is not to be used, left to configure
-export const global_state = $state({
+export const global_state = $state<{
+	isModelLoaded: boolean,
+	data: any[],
+	embed_output: ScatterPlotData,
+	tokens: any[],
+	active_head: number,
+	active_block: number
+}>({
 	isModelLoaded: false,
 	data: [],
+	embed_output: [],
 	tokens: [],
 	active_head: 0,
 	active_block: 0,
