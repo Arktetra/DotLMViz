cb52c204bbaa6cc12deec6a636fe7a5ac25b3e44
Arktetra <sameershrestha.sm@gmail.com>
2025-02-02 09:03:44 +0545

    minor changes

--- src/DoTLMViz/kde.py
+++ src/DoTLMViz/kde.py
@@ -1,6 +1,6 @@
 import torch
 
-from typing import Callable
+from typing import Callable, List
 
 from .utils import convert_to_points
 
@@ -13,11 +13,31 @@ def epanechnikov(v: torch.Tensor, h: float):
 
 
 class KernelDensityEstimator:
+    """
+    A kernel density estimator that will be created according to the kernel
+    function and bandwidth passed.
+    """
+
     def __init__(self, kernel: Callable = epanechnikov, bandwidth: float = 0.17):
         self.kernel = kernel
         self.bandwidth = bandwidth
 
-    def estimate(self, data: torch.Tensor, start: float, end: float, steps: int = None):
+    def estimate(self, data: torch.Tensor, start: float, end: float, steps: int = None) -> List[List[float]]:
+        """Estimates the probability density function of the data, and returns
+        a list of points consisting of the probability density for the provided
+        range.
+
+        Args:
+            data (torch.Tensor): the data of which the probability density is to be estimated.
+            start (float): the starting value of the x values.
+            end (float): the ending value of the x values.
+            steps (int, optional): number of points between start and end. Defaults to None.
+
+        Returns:
+            List[List[float]]: A list of points consisting of the probability density
+            for the provided range.
+        """
+
         steps = 1 if steps is None else steps
         x = torch.linspace(start, end, steps).reshape(-1, 1, 1)
         y = self.kernel(x - data, self.bandwidth).mean(dim=[1, 2])
