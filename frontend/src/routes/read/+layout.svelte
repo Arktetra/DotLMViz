701e88ea3850a35ff009c7e104cbde2bb97c132f
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-02-15 16:32:53 +0545

    add: reading section page & layouts

---
+++ frontend/src/routes/read/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    const { children } = $props()
+    import { content } from '$lib/content';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+    import { fly } from 'svelte/transition';
+
+    const getTitle = () => {
+        const temp: string[] = []
+        Object.entries(content).forEach(([title, content]) => {
+            temp.push(title)
+        })
+        return temp;
+    }
+
+    let navState = $state<boolean>(true)
+
+    const navToggle = (s?: boolean) => {
+        if(s) navState = s;
+        navState = !navState;
+    }
+
+    let activeInd = $state(0)
+</script>
+
+<button
+    onclick={() => navToggle()}
+    class="fixed top-4 left-4 z-50 bg-theme-w rounded-md text-theme hover:bg-theme-g border border-theme cursor-pointer"
+>
+    <ArrowRightOutline class="w-7 h-7 transition-transform duration-300 {navState ? "rotate-180" : "rotate-0"}" />
+</button>
+{#if navState}
+<nav 
+    transition:fly={{duration: 300}}
+    class="fixed h-full sm:w-[20rem] w-[17rem] top-0 left-0 flex flex-col justify-center items-center text-left bg-theme border-r shadow-xl"
+>
+    {#each getTitle() as c, ind}
+        <a 
+            href="/read/{c}"
+            onclick={() => activeInd = ind}
+            class="w-full p-4 hover:px-6 text-theme-w  hover:bg-theme-alt {activeInd == ind ? "border-l-8 border-theme-w font-extrabold" :""} capitalize transition-all duration-300"
+        >
+            {ind+1 + ". " + c}
+        </a>
+    {/each}
+</nav>
+{/if}
+<section class="w-full min-h-screen flex flex-col justify-start items-center bg-theme-w transition-all duration-300 {navState ? "pl-[20rem]" : "p-0"}">
+    {#if children}
+        {@render children()}
+    {/if}
+</section>
--- frontend/src/routes/read/[title]/+page.svelte
+++ frontend/src/routes/read/[title]/+page.svelte
@@ -3,20 +3,17 @@
 	import { content } from '$lib/content';
 
 	console.log(page.params);
-	const title = page.params.title.split('-').join(' ');
-	const description = content[page.params.title] ?? 'NO information available for this topic!!';
+	const title = $derived(page.params.title.split('-').join(' '));
+	const description = $derived(content[page.params.title] ?? 'NO information available for this topic!!');
 </script>
 
-<div class="flex flex-col items-center justify-evenly pt-10 font-main-a">
-	<span class="text-md uppercase">Readings</span>
-	<div
-		class="my-10 min-h-[80vh] w-[80%] rounded-sm border-2 border-theme-g p-10 text-justify text-lg tracking-wider shadow-lg"
-	>
-		<h1 class="mb-10 text-center font-main text-4xl font-extrabold uppercase text-theme underline">
-			{title}
-		</h1>
-		<p class="h-full w-full">
-			{@html description}
-		</p>
-	</div>
+<div
+	class="my-10 md:max-w-[80%] max-w-[90%] min-h-[50vh] bg-theme-g border rounded-sm text-justify text-lg tracking-wider"
+>
+	<h1 class="py-5 text-center font-main-a text-2xl font-extrabold uppercase text-theme-w bg-theme">
+		{title}
+	</h1>
+	<p class="px-10 my-5">
+		{@html description}
+	</p>
 </div>
