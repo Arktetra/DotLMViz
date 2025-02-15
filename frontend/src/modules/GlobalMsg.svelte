5a75788838c258d540dc9aafb4dbe183b0d5e4e9
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-02-15 09:56:16 +0545

    add: GlobalMessage Module (only error configed)

---
+++ frontend/src/modules/GlobalMsg.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Message from "../components/Message.svelte";
+    import { global_state } from "../state.svelte";
+
+    let infoInt: any = null;
+
+    $effect(() => {
+        if(global_state.info && !infoInt)
+        {
+            infoInt = setTimeout(() => {
+                global_state.info = null;
+                infoInt = null;
+                // console.log("Timeout for message")
+            }, 4000)
+        }
+    })
+
+    const closeMsg = () => {
+        if(infoInt) {
+            clearInterval(infoInt)
+            global_state.info = null;
+            infoInt = null;
+        }
+    }
+</script>
+
+{#if global_state.info}
+    <Message message={global_state.info?.message} type={global_state.info?.code}>
+        <button onclick={() => closeMsg()} class="hover:scale-125 text-sm ml-5">X</button>
+    </Message>
+{/if}
